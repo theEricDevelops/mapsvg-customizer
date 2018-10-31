1cae849f580b31d4807ed41a2549bbaaa1194774
Eric Baker <eric@ericbaker.me>
2018-10-31 13:17:12 -0400

    Shows list of articles by county when a region is clicked.

--- assets/js/mapsvg_js.js
+++ assets/js/mapsvg_js.js
@@ -1,20 +1,36 @@
 var mapsvg_customizer = jQuery( document ).ready( function ( e ) {
 	e("path").on("click", function( i ) {
+        var output = "";
+
         var targetTax = i.target.id.toString().toLowerCase().replace(/[^a-z\s]+/g, '').replace(/\s+/g, '-');
         targetTax = targetTax + '-county';
-        console.log(targetTax);
 
         var reqBaseUrl = '/wp-content/plugins/mapsvg-customizer/includes/class-mapsvg-customizer-data.php';
         var reqUrl = reqBaseUrl + '?t=' + targetTax;
-        console.log(reqUrl);
 
         var request = e.ajax({
-            url:            reqUrl,
-            crossDomain:    false,
-            dataType:       JSON,
-        }).done(function(){
-            alert("Success!");
+            type: 'GET',
+            dataType: 'json',
+            url: reqUrl,
+            timeout: 5000,
+            success: function(data, textStatus){
+                if(data === undefined || data.length == 0){
+                    output = "There are no articles for this region.";
+                    e("div.mapsvg-controller-view-content").html(output);
+                } else {
+                    output = '<ul class="county-article-list">';
+                    data.forEach(function(val){
+                        output += '<li class="county-article-link">';
+                        output += '<a href="' + val.link + '" title="' + val.title + '">' + val.title + '</a>';
+                        output += '</li>';
+                    });
+                    output += '</ul>';
+                    e("div.mapsvg-controller-view-content").html(output);
+                }
+            },
+            fail: function(xhr, textStatus, errorThrown){
+                console.log(errorThrown);
+            }
         });
-        console.log(request);
     })
 });
