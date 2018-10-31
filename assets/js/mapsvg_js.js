2b44e006b4ecb7db6e50361559707e89b745722a
Eric Baker <eric@ericbaker.me>
2018-10-31 12:34:26 -0400

    Sets up the ajax request for articles

--- assets/js/mapsvg_js.js
+++ assets/js/mapsvg_js.js
@@ -1,5 +1,20 @@
 var mapsvg_customizer = jQuery( document ).ready( function ( e ) {
-	e("path").on("click", function() {
-        
+	e("path").on("click", function( i ) {
+        var targetTax = i.target.id.toString().toLowerCase().replace(/[^a-z\s]+/g, '').replace(/\s+/g, '-');
+        targetTax = targetTax + '-county';
+        console.log(targetTax);
+
+        var reqBaseUrl = '/wp-content/plugins/mapsvg-customizer/includes/class-mapsvg-customizer-data.php';
+        var reqUrl = reqBaseUrl + '?t=' + targetTax;
+        console.log(reqUrl);
+
+        var request = e.ajax({
+            url:            reqUrl,
+            crossDomain:    false,
+            dataType:       JSON,
+        }).done(function(){
+            alert("Success!");
+        });
+        console.log(request);
     })
 });
