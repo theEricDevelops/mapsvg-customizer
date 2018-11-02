c91f966d3f120108602308e26386905016869f17
e-baker <eric@trafficlight.me>
2018-11-02 12:17:11 -0400

    Uses articles JSON to show links on popover

--- assets/js/msvgc_json.js
+++ assets/js/msvgc_json.js
@@ -1,9 +1,11 @@
 var mapsvgc_json = jQuery( document ).ready( function ( e ) {
+    var articles = Array();
     var jsonFile = new XMLHttpRequest();
     jsonFile.open('GET', '/wp-content/plugins/mapsvg-customizer/articles.json', true);
     jsonFile.onreadystatechange = function() {
         if ( jsonFile.readyState === 4 && jsonFile.status === 200 )  {
             var articleJSON = jsonFile.responseText;
+            articles = JSON.parse(articleJSON);
         }
     }
     jsonFile.send(null);
@@ -14,34 +16,29 @@ var mapsvgc_json = jQuery( document ).ready( function ( e ) {
         var targetTax = i.target.id.toString().toLowerCase().replace(/[^a-z\s]+/g, '').replace(/\s+/g, '-');
         targetTax = targetTax + '-county';
 
-        // OLD CODE BELOW
-
-        var reqBaseUrl = '/wp-content/plugins/mapsvg-customizer/includes/class-mapsvg-customizer-api.php';
-        var reqUrl = reqBaseUrl + '?t=' + targetTax;
-
-        var request = e.ajax({
-            type: 'GET',
-            dataType: 'json',
-            url: reqUrl,
-            timeout: 5000,
-            success: function(data, textStatus){
-                if( data.constructor !== Array || data.length < 1 ) {
-                    output = "There are no articles for this region.";
-                    e("div.mapsvg-controller-view-content").html(output);
-                } else {
-                    output = '<ul class="county-article-list">';
-                    data.forEach(function(val){
-                        output += '<li class="county-article-link">';
-                        output += '<a href="' + val.link + '" title="' + val.title + '">' + val.title + '</a>';
-                        output += '</li>';
-                    });
-                    output += '</ul>';
-                    e("div.mapsvg-controller-view-content").html(output);
+        function getArticleLinks() {
+            var links = Array();
+            articles.forEach(function(val) {
+                if(val.county == targetTax) {
+                    var href = '<a href="' + val.link + '" title="' + val.title + '">' + val.title + '</a>';
+                    console.log(href);
+                    links.push(href);
                 }
-            },
-            fail: function(xhr, textStatus, errorThrown){
-                console.log(errorThrown);
-            }
-        });
+            });
+            return links;
+        }
+
+        var articleLinks = getArticleLinks();
+        if( articleLinks.length == 0 ) { 
+            output = "There are no articles for this region.";
+            e("div.mapsvg-controller-view-content").html(output);
+        } else {
+            output = '<ul class="county-article-list">';
+            articleLinks.forEach(function(val){
+                output += '<li class="county-article-link">' + val + '</li>';
+            });
+            output += '</ul>';
+            e("div.mapsvg-controller-view-content").html(output);
+        }
     })
 });
